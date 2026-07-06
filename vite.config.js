4021eb02e26efcfc0dc17e7c9248d2fa3e36f88a
Kavitha <selvikavitha362@gmail.com>
2026-07-06 22:14:52 +0530

    deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+<<<<<<< HEAD
   base: "/fitness-challenge-portal/",
+=======
+  base: "/fitness-challenge-tracker/",
+>>>>>>> 67ab4b4 (deployment issue)
 });
